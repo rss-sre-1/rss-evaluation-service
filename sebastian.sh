7c711d0b470e2ddf00a62c6c28cd5a4fb004f5b1
joneschmitz <joneschmitz@gmail.com>
2021-04-06 17:34:59 -0500

    Resolved empty file problem.

--- sebastian.sh
+++ sebastian.sh
@@ -2,35 +2,44 @@
 Org="rss-sre-1";
 Repo="rss-evaluation-service";
 URL="https://github.com/$Org/$Repo/commits/dev";
+GrepURL="https://github.com/$Org/$Repo/commit/";
 BaseFile="11111111112";
 TempFile="11111111111";
+GatherFile="11111111113";
 Interval="1m";
 
 rm $TempFile &> /dev/null;
 rm $BaseFile &> /dev/null;
+echo "" > $TempFile;
+echo "" > $BaseFile;
 
-curl $URL -o $BaseFile &> /dev/null;
-cat $BaseFile | grep "https://github.com/$Org/$Repo/commit/" > $BaseFile; # get all commits
-
+curl $URL -s -o $BaseFile; # 2> /dev/null;
+cat $BaseFile | grep $GrepURL > $GatherFile; # get all commits
+mv $GatherFile $BaseFile;
 
 stop=$(cat stop 2> /dev/null);
 
 # The loop does not stop until there is a file named 'stop' with content in it.
 while [ "$stop" == "" ]; do
   echo $(date +"%T") - ;
-  curl $URL -o $TempFile &> /dev/null;
-  cat $TempFile | grep "https://github.com/$Org/$Repo/commit/" > $TempFile; # get all current commits
+  curl $URL -s -o $TempFile; # 2> /dev/null;
+  cat $TempFile | grep $GrepURL > $GatherFile; # get all current commits
+  mv $GatherFile $TempFile;
 
+  echo "-- $(cat $BaseFile) --";
+  echo "-- $(cat $TempFile) --";
   # Compare past and current commits
+
+#read -p "continue?"
+
   if [ "$(diff $BaseFile $TempFile)" != "" ]; then
     echo "Change found, starting mini-pipeline.";
     mv $TempFile $BaseFile; # Update base commit list.
     ./MiniPipeline.sh;
   fi
-  rm $TempFile &> /dev/null;
   stop=$(cat stop 2> /dev/null);
   echo " - ";
   sleep $Interval;
 done
-
-rm $BaseFile;
+rm $TempFile &> /dev/null;
+rm $BaseFile &> /dev/null;
