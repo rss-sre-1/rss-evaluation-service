770ce10e12f369ad74032978658eca4302353e81
joneschmitz <joneschmitz@gmail.com>
2021-04-08 19:21:57 -0500

    Update sebastian script
    Added commit persistence while after being stopped. Improved stop response time. Added verification for removing stop file.

--- sebastian.sh
+++ sebastian.sh
@@ -1,46 +1,64 @@
 #!/bin/bash
+#For this program to run the pipeline, a MiniPipeline.sh file must be present in the same directory as this file.
+
 Org="rss-sre-1";
 Repo="rss-evaluation-service";
-URL="https://github.com/$Org/$Repo/commits/dev";
-GrepURL="https://github.com/$Org/$Repo/commit/";
-BaseFile="11111111112";
-TempFile="11111111111";
-GatherFile="11111111113";
 Interval="1m";
 
+#Only change if you already have files with these names
+GatherFile="11111111113";
+BaseFile="11111111112";
+TempFile="11111111111";
+
+#Do not change the following variables
+URL="https://github.com/$Org/$Repo/commits/dev";
+GrepURL="https://github.com/$Org/$Repo/commit/";
+
+# CHECKING FOR BASE FILE STORING OLD COMMIT
 rm $TempFile &> /dev/null;
-rm $BaseFile &> /dev/null;
+if [ "$(ls | grep $BaseFile 2> /dev/null)" != "$BaseFile" ]; then
+#rm $BaseFile &> /dev/null;
+  echo "" > $BaseFile;
+  curl $URL -s -o $BaseFile; # 2> /dev/null;
+  cat $BaseFile | grep $GrepURL > $GatherFile; # get initial commit
+  mv $GatherFile $BaseFile;
+
+else
+  echo "Previous commit found.";
+fi
 echo "" > $TempFile;
-echo "" > $BaseFile;
 
-curl $URL -s -o $BaseFile; # 2> /dev/null;
-cat $BaseFile | grep $GrepURL > $GatherFile; # get all commits
-mv $GatherFile $BaseFile;
-
+# CHECKING FOR STOP FILE
+if [ "$(ls | grep stop 2> /dev/null)" == "stop" ]; then
+  read -p "File 'stop' found. Remove and continue? (y/n): ";
+  if [ "$REPLY" != "y" ]; then
+    exit;
+  else
+    rm stop &> /dev/null;
+  fi
+fi
 stop=$(cat stop 2> /dev/null);
 
 # The loop does not stop until there is a file named 'stop' with content in it.
-# Or it will stop when you enter stop="stop" in another terminal.
 while [ "$stop" == "" ]; do
   echo $(date +"%T") - ;
+
+  # GET CURRENT COMMIT
   curl $URL -s -o $TempFile; # 2> /dev/null;
-  cat $TempFile | grep $GrepURL > $GatherFile; # get all current commits
+  cat $TempFile | grep $GrepURL > $GatherFile;
   mv $GatherFile $TempFile;
 
-#  echo "-- $(cat $BaseFile) --";
-#  echo "-- $(cat $TempFile) --";
-  # Compare past and current commits
-
-#read -p "continue?"
-
+  # CHECK IF NEW COMMIT DOES NOT MATCH OLD COMMIT
   if [ "$(diff $BaseFile $TempFile)" != "" ]; then
+    #read -p "continue?"
     echo "Change found, starting mini-pipeline.";
-    mv $TempFile $BaseFile; # Update base commit list.
+    mv $TempFile $BaseFile;
     ./MiniPipeline.sh;
   fi
-  stop=$(cat stop 2> /dev/null);
+
   echo " - ";
   sleep $Interval;
+  stop=$(cat stop 2> /dev/null);
 done
+
 rm $TempFile &> /dev/null;
-rm $BaseFile &> /dev/null;
