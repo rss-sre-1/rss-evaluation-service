b61a46be222ffb5fb7152a63fc46937d9ac063c1
joneschmitz <joneschmitz@gmail.com>
2021-04-12 21:18:23 -0500

    Simplified Sebastian, shifting more control to pipeline.

---
+++ Pipeline.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#PIPELINE VARIABLES
+Namespace="rss-evaluation"
+
+ImageRegistry="855430746673.dkr.ecr.us-east-1.amazonaws.com";
+ImageName="matt-oberlies-sre-p3-rss-evaluation";
+ImageTag="latest";
+DockerfileLocation="/Evaluation/";
+
+DeploymentName="rss-evaluation-deployment";
+########################### THE RELATIVE PATH + MANIFEST FILENAME.
+DeploymentManifestLocation="rss-evaluation-manifests/rss-evaluation-deployment.yaml";
+
+#COMMIT PROBE SETTINGS
+Org="rss-sre-1";
+Repo="rss-evaluation-service";
+#Interval="1m";
+
+
+#ONLY CHANGE IF YOU ALREADY HAVE FILES WITH THESE NAMES WITH DATA UNRELATED TO THE PIPELINE
+GatherFile="11111111113";
+BaseFile="11111111112";
+TempFile="11111111111";
+
+#DO NOT CHANGE THESE VARIABLES
+URL="https://github.com/$Org/$Repo/commits/dev";
+GrepURL="https://github.com/$Org/$Repo/commit/";
+
+# CHECKING FOR BASE FILE STORING OLD COMMIT
+rm $TempFile &> /dev/null;
+if [ "$(ls | grep $BaseFile 2> /dev/null)" != "$BaseFile" ]; then
+  echo "" > $BaseFile;
+  curl $URL -s -o $BaseFile; # 2> /dev/null;
+  cat $BaseFile | grep $GrepURL > $GatherFile; # get initial commit
+  mv $GatherFile $BaseFile;
+#else
+#  echo "Previous commit found.";
+fi
+echo "" > $TempFile;
+
+
+# GET CURRENT COMMIT
+  curl $URL -s -o $TempFile; # 2> /dev/null;
+  cat $TempFile | grep $GrepURL > $GatherFile;
+  mv $GatherFile $TempFile;
+
+# CHECK IF NEW COMMIT DOES NOT MATCH OLD COMMIT
+if [ "$(diff $BaseFile $TempFile)" != "" ]; then
+  #read -p "continue?"
+  echo "Change found, starting mini-pipeline.";
+  #mv $TempFile $BaseFile;
+
+#STARTING PIPELINE (you can add/modify/remove stages)
+  aws ecr get-login-password --profile sre | docker login --username AWS --password-stdin 855430746673.dkr.ecr.us-east-1.amazonaws.com;
+  git pull;
+
+  touch DF;
+  err="$(docker build . -f DF 2>&1)";
+  rm DF;
+  if [[ "${err:0:5}" != "error" && "${err:0:6}" != "unable" ]]; then
+#BUILD STAGE
+    docker build -t $ImageRegistry/$ImageName:$ImageTag .$DockerfileLocation;
+    docker push $ImageRegistry/$ImageName:$ImageTag;
+
+#DEPLOYMENT STAGE
+    kubectl -n $Namespace delete deployment $DeploymentName;
+    kubectl -n $Namespace apply -f $DeploymentManifestLocation;
+
+
+  mv $TempFile $BaseFile;
+  else
+    echo "$err"
+  fi
+
+fi
